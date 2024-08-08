cb7fca8dad13af83a9d04871f6059ef8f009ea3c
Sergei Demidov <Salvan26@mail.ru>
2024-08-09 01:39:56 +0300

    font-add

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,17 @@ import '../scss/desktop.scss'
 import './brand-swiper.js'
 import './hardware-swiper.js'
 import './services-swiper.js'
+
+const burger = document.querySelector('.round-icon--menu')
+burger.addEventListener('click', function () {
+  let content = document.querySelector('.side-menu')
+  content.classList.remove('menu--off')
+  document.documentElement.style.overflow = 'hidden'
+})
+
+const open = document.querySelector('.open--menu')
+open.addEventListener('click', function () {
+  let content = document.querySelector('.side-menu')
+  content.classList.add('menu--off')
+  document.documentElement.style.overflow = 'auto'
+})
