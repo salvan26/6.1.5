e2520be72f042d666140824e71987f81fcb8504b
Sergei Demidov <Salvan26@mail.ru>
2024-08-11 00:27:47 +0300

    last commit

--- src/js/index.js
+++ src/js/index.js
@@ -27,27 +27,74 @@ open.addEventListener('click', function () {
 const overlayClose = function (str) {
   let overlay = document.querySelector('.overlay')
   overlay.addEventListener('click', function () {
-    console.log('click')
     let content = str
     content.classList.add('menu--off')
     overlay.classList.remove('overlay--on')
+    document.documentElement.style.overflow = 'auto'
   })
 }
-const call = document.querySelector('.button-call')
+const call = document.querySelector('.open--call')
 call.addEventListener('click', function () {
   let content = document.querySelector('.modal-callback')
   overlayClose(content)
   content.classList.remove('menu--off')
+  let side = document.querySelector('.side-menu')
+  side.classList.add('menu--off')
   document.documentElement.style.overflow = 'hidden'
   let overlay = document.querySelector('.overlay')
   overlay.classList.add('overlay--on')
 })
 
-const close = document.querySelector('.modal__off')
+const close = document.querySelector('.modal-callback__off')
 close.addEventListener('click', function () {
   let content = document.querySelector('.modal-callback')
   content.classList.add('menu--off')
   document.documentElement.style.overflow = 'auto'
   let overlay = document.querySelector('.overlay')
   overlay.classList.remove('overlay--on')
+})
+
+const chat = document.querySelector('.open--chat')
+chat.addEventListener('click', function () {
+  let content = document.querySelector('.modal-feedback')
+  overlayClose(content)
+  content.classList.remove('menu--off')
+  let side = document.querySelector('.side-menu')
+  side.classList.add('menu--off')
+  document.documentElement.style.overflow = 'hidden'
+  let overlay = document.querySelector('.overlay')
+  overlay.classList.add('overlay--on')
+})
+
+const closeChat = document.querySelector('.modal-feedback__off')
+closeChat.addEventListener('click', function () {
+  let content = document.querySelector('.modal-feedback')
+  content.classList.add('menu--off')
+  document.documentElement.style.overflow = 'auto'
+  let overlay = document.querySelector('.overlay')
+  overlay.classList.remove('overlay--on')
+})
+
+const calltab = document.querySelector('.button-call')
+calltab.addEventListener('click', function () {
+  let content = document.querySelector('.modal-callback')
+  overlayClose(content)
+  content.classList.remove('menu--off')
+  let side = document.querySelector('.side-menu')
+  side.classList.add('menu--off')
+  document.documentElement.style.overflow = 'hidden'
+  let overlay = document.querySelector('.overlay')
+  overlay.classList.add('overlay--on')
+})
+
+const chattab = document.querySelector('.button-chat')
+chattab.addEventListener('click', function () {
+  let content = document.querySelector('.modal-callback')
+  overlayClose(content)
+  content.classList.remove('menu--off')
+  let side = document.querySelector('.side-menu')
+  side.classList.add('menu--off')
+  document.documentElement.style.overflow = 'hidden'
+  let overlay = document.querySelector('.overlay')
+  overlay.classList.add('overlay--on')
 })
