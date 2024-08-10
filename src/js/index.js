f39e8ed819e5bcabf949e63ef0782169af402f94
Sergei Demidov <Salvan26@mail.ru>
2024-08-10 04:53:46 +0300

    11

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,7 @@ import './services-swiper.js'
 const burger = document.querySelector('.round-icon--menu')
 burger.addEventListener('click', function () {
   let content = document.querySelector('.side-menu')
+  overlayClose(content)
   content.classList.remove('menu--off')
   document.documentElement.style.overflow = 'hidden'
   let overlay = document.querySelector('.overlay')
@@ -23,22 +24,30 @@ open.addEventListener('click', function () {
   let overlay = document.querySelector('.overlay')
   overlay.classList.remove('overlay--on')
 })
-
-let overlay = document.querySelector('.overlay')
-overlay.addEventListener('click', function () {
-  let content = document.querySelector('.side-menu')
-  content.classList.add('menu--off')
-  overlay.classList.remove('overlay--on')
+const overlayClose = function (str) {
+  let overlay = document.querySelector('.overlay')
+  overlay.addEventListener('click', function () {
+    console.log('click')
+    let content = str
+    content.classList.add('menu--off')
+    overlay.classList.remove('overlay--on')
+  })
+}
+const call = document.querySelector('.button-call')
+call.addEventListener('click', function () {
+  let content = document.querySelector('.modal-callback')
+  overlayClose(content)
+  content.classList.remove('menu--off')
+  document.documentElement.style.overflow = 'hidden'
+  let overlay = document.querySelector('.overlay')
+  overlay.classList.add('overlay--on')
 })
 
-const readMore = document.querySelector('.main__readmore-block')
-readMore.addEventListener('click', function () {
-  let content = document.querySelector('.main__info')
-  if (content.classList.contains('wrapper-show')) {
-    content.classList.remove('wrapper-show')
-    showMore.textContent = 'Показать все'
-  } else {
-    content.classList.add('wrapper-show')
-    showMore.textContent = 'Скрыть'
-  }
+const close = document.querySelector('.modal__off')
+close.addEventListener('click', function () {
+  let content = document.querySelector('.modal-callback')
+  content.classList.add('menu--off')
+  document.documentElement.style.overflow = 'auto'
+  let overlay = document.querySelector('.overlay')
+  overlay.classList.remove('overlay--on')
 })
